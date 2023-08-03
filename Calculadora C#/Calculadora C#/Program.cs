6330c10a00a575731cb6949f5a61d2f81def95e9
Gabriel Breder <83596196+GabrielBreder33@users.noreply.github.com>
2023-08-03 14:06:17 -0300

    Add files via upload

---
+++ Calculadora C#/Calculadora C#/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace Calculadora
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Calculadora Simples");
+            Console.WriteLine("-------------------");
+            Console.WriteLine("Digite o primeiro número:");
+            double num1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Digite o operador (+, -, *, /):");
+            char operador = Convert.ToChar(Console.ReadLine());
+
+            Console.WriteLine("Digite o segundo número:");
+            double num2 = Convert.ToDouble(Console.ReadLine());
+
+            double resultado = 0;
+
+            switch (operador)
+            {
+                case '+':
+                    resultado = num1 + num2;
+                    break;
+                case '-':
+                    resultado = num1 - num2;
+                    break;
+                case '*':
+                    resultado = num1 * num2;
+                    break;
+                case '/':
+                    if (num2 != 0)
+                    {
+                        resultado = num1 / num2;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Erro: Divisão por zero não é permitida.");
+                        return;
+                    }
+                    break;
+                default:
+                    Console.WriteLine("Operador inválido.");
+                    return;
+            }
+
+            Console.WriteLine("Resultado: " + resultado);
+        }
+    }
+}
